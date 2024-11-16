107551a44cdbec456c6da515822c4c672425db0b
Berto Mill <bertmill19@gmail.com>
2024-11-15 20:24:52 -0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 // src/app/page.tsx
+
 import Dashboard from "@/components/meal-planner/Dashboard";
 
-export default function HomePage() {
+export default function Home() {
   return (
     <main>
       <Dashboard />
